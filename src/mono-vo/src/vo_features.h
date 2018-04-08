61d6e57f0525bd92a2e8e9ad4e376e538a5d3b44
shubh-agrawal <shubh.agrawal111@gmail.com>
2018-04-08 22:13:21 +0530

    code optimization

--- src/mono-vo/src/vo_features.h
+++ src/mono-vo/src/vo_features.h
@@ -1,10 +1,7 @@
-#include "opencv2/video/tracking.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/features2d/features2d.hpp"
-#include "opencv2/xfeatures2d.hpp"
-#include "opencv2/calib3d/calib3d.hpp"
-#include "opencv2/core/version.hpp"
+#include "opencv2/opencv.hpp"
 #include "opencv2/viz.hpp"
 
 #include <iostream>
@@ -17,20 +14,55 @@
 #include <sstream>
 #include <fstream>
 #include <string>
+#include <numeric>
+#include "range-tree/anms.h"
+
+
+#include <gtsam/geometry/Point2.h>
+#include <gtsam/inference/Symbol.h>
+#include <gtsam/slam/PriorFactor.h>
+#include <gtsam/slam/ProjectionFactor.h>
+#include <gtsam/slam/GeneralSFMFactor.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
+#include <gtsam/nonlinear/DoglegOptimizer.h>
+#include <gtsam/nonlinear/Values.h>
+
 
 using namespace cv;
 using namespace std;
-using namespace cv::xfeatures2d;
 
-const double nn_match_ratio = 0.8f;
+const double nn_match_ratio = 0.7f;
+
+// void featureDetection(Mat img_1, vector<KeyPoint>& points1)
+// { 
+//   // int fast_threshold = 20;
+//   // bool nonmaxSuppression = true;
+//   Ptr<AKAZE> detector = AKAZE::create();
+//   detector->detect(img_1, points1); 
+// }
+
 
 void featureDetection(Mat img_1, vector<KeyPoint>& points1)
 { 
-  // int fast_threshold = 20;
-  // bool nonmaxSuppression = true;
-  Ptr<AKAZE> detector = AKAZE::create();
-  detector->detect(img_1, points1, 3e-4); 
+    vector<cv::KeyPoint> keyPoints;
+    //FAST(img_1,keyPoints,10,true);
+    Ptr<AKAZE> detector = AKAZE::create();
+    detector->detect(img_1, keyPoints); 
+
+    vector<int> responseVector;
+    for (unsigned int i =0 ; i<keyPoints.size(); i++) responseVector.push_back(keyPoints[i].response);
+    vector<int> Indx(responseVector.size()); std::iota (std::begin(Indx), std::end(Indx), 0);
+    cv::sortIdx(responseVector, Indx, CV_SORT_DESCENDING);
+    vector<cv::KeyPoint> keyPointsSorted;
+    for (unsigned int i = 0; i < keyPoints.size(); i++) keyPointsSorted.push_back(keyPoints[Indx[i]]);
+
+    int numRetPoints = 500;
+    vector<cv::KeyPoint> brownKP = BrownANMS(keyPointsSorted,numRetPoints);
+    points1 = brownKP;
+
 }
+
 
 void descriptorDetection(Mat img_1, Mat& descriptors1, vector<KeyPoint>& points1)
 {
@@ -45,7 +77,7 @@ bool compare_dist(DMatch first, DMatch second)
   else return false;
 }
 
-void featureTrackingFlann(Mat img_1, Mat img_2, vector<KeyPoint>& points1, vector<KeyPoint>& points2, Mat& descriptors1, Mat& descriptors2)
+void featureTrackingFlann(Mat img_1, Mat img_2, vector<KeyPoint>& points1, vector<KeyPoint>& points2, Mat& descriptors1, Mat& descriptors2, vector<int>& kp1_used, vector<int>& kp2_used)
 {
 
     std::vector< vector<DMatch> > matches; // change to <DMatch> when not using knn
@@ -63,6 +95,8 @@ void featureTrackingFlann(Mat img_1, Mat img_2, vector<KeyPoint>& points1, vecto
         {
             tempPoints1.push_back(points1[matches[i][0].queryIdx]);
             tempPoints2.push_back(points2[matches[i][0].trainIdx]);
+            kp1_used.push_back(matches[i][0].queryIdx);
+            kp2_used.push_back(matches[i][0].trainIdx);
             good_matches.push_back(matches[i][0]);
         }
     }
@@ -83,10 +117,193 @@ void featureTrackingFlann(Mat img_1, Mat img_2, vector<KeyPoint>& points1, vecto
                vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 
     imshow("Good matches", img_matches);
-    cout << "Total features: " << matches.size() << "  ";
-    cout << "First image: " << tempPoints1.size() << "  "; 
-    cout << "Second image: " << tempPoints2.size() << endl;
+    
+    // cout << "Total features: " << matches.size() << "  ";
+    // cout << "First image: " << tempPoints1.size() << "  "; 
+    // cout << "Second image: " << tempPoints2.size() << endl;
 
     points1 = tempPoints1; points2 = tempPoints2;
 
+}
+
+
+
+// Converts a given Rotation Matrix to Euler angles
+cv::Mat rot2euler(const cv::Mat & rotationMatrix)
+{
+  cv::Mat euler(3,1,CV_64F);
+
+  double m00 = rotationMatrix.at<double>(0,0);
+  double m02 = rotationMatrix.at<double>(0,2);
+  double m10 = rotationMatrix.at<double>(1,0);
+  double m11 = rotationMatrix.at<double>(1,1);
+  double m12 = rotationMatrix.at<double>(1,2);
+  double m20 = rotationMatrix.at<double>(2,0);
+  double m22 = rotationMatrix.at<double>(2,2);
+
+  double x, y, z;
+
+  // Assuming the angles are in radians.
+  if (m10 > 0.998) { // singularity at north pole
+    x = 0;
+    y = CV_PI/2;
+    z = atan2(m02,m22);
+  }
+  else if (m10 < -0.998) { // singularity at south pole
+    x = 0;
+    y = -CV_PI/2;
+    z = atan2(m02,m22);
+  }
+  else
+  {
+    x = atan2(-m12,m11);
+    y = asin(m10);
+    z = atan2(-m20,m00);
+  }
+
+  euler.at<double>(0) = x;
+  euler.at<double>(1) = y;
+  euler.at<double>(2) = z;
+
+  return euler;
+}
+
+// Converts a given Euler angles to Rotation Matrix
+cv::Mat euler2rot(const cv::Mat & euler)
+{
+  cv::Mat rotationMatrix(3,3,CV_64F);
+
+  double x = euler.at<double>(0);
+  double y = euler.at<double>(1);
+  double z = euler.at<double>(2);
+
+  // Assuming the angles are in radians.
+  double ch = cos(z);
+  double sh = sin(z);
+  double ca = cos(y);
+  double sa = sin(y);
+  double cb = cos(x);
+  double sb = sin(x);
+
+  double m00, m01, m02, m10, m11, m12, m20, m21, m22;
+
+  m00 = ch * ca;
+  m01 = sh*sb - ch*sa*cb;
+  m02 = ch*sa*sb + sh*cb;
+  m10 = sa;
+  m11 = ca*cb;
+  m12 = -ca*sb;
+  m20 = -sh*ca;
+  m21 = sh*sa*cb + ch*sb;
+  m22 = -sh*sa*sb + ch*cb;
+
+  rotationMatrix.at<double>(0,0) = m00;
+  rotationMatrix.at<double>(0,1) = m01;
+  rotationMatrix.at<double>(0,2) = m02;
+  rotationMatrix.at<double>(1,0) = m10;
+  rotationMatrix.at<double>(1,1) = m11;
+  rotationMatrix.at<double>(1,2) = m12;
+  rotationMatrix.at<double>(2,0) = m20;
+  rotationMatrix.at<double>(2,1) = m21;
+  rotationMatrix.at<double>(2,2) = m22;
+
+  return rotationMatrix;
+}
+
+
+void initKalmanFilter(cv::KalmanFilter &KF, int nStates, int nMeasurements, int nInputs, double dt)
+{
+  KF.init(nStates, nMeasurements, nInputs, CV_64F);                 // init Kalman Filter
+  cv::setIdentity(KF.processNoiseCov, cv::Scalar::all(1e-5));       // set process noise
+  cv::setIdentity(KF.measurementNoiseCov, cv::Scalar::all(1e-4));   // set measurement noise
+  cv::setIdentity(KF.errorCovPost, cv::Scalar::all(1));             // error covariance
+                 /* DYNAMIC MODEL */
+  //  [1 0 0 dt  0  0 dt2   0   0 0 0 0  0  0  0   0   0   0]
+  //  [0 1 0  0 dt  0   0 dt2   0 0 0 0  0  0  0   0   0   0]
+  //  [0 0 1  0  0 dt   0   0 dt2 0 0 0  0  0  0   0   0   0]
+  //  [0 0 0  1  0  0  dt   0   0 0 0 0  0  0  0   0   0   0]
+  //  [0 0 0  0  1  0   0  dt   0 0 0 0  0  0  0   0   0   0]
+  //  [0 0 0  0  0  1   0   0  dt 0 0 0  0  0  0   0   0   0]
+  //  [0 0 0  0  0  0   1   0   0 0 0 0  0  0  0   0   0   0]
+  //  [0 0 0  0  0  0   0   1   0 0 0 0  0  0  0   0   0   0]
+  //  [0 0 0  0  0  0   0   0   1 0 0 0  0  0  0   0   0   0]
+  //  [0 0 0  0  0  0   0   0   0 1 0 0 dt  0  0 dt2   0   0]
+  //  [0 0 0  0  0  0   0   0   0 0 1 0  0 dt  0   0 dt2   0]
+  //  [0 0 0  0  0  0   0   0   0 0 0 1  0  0 dt   0   0 dt2]
+  //  [0 0 0  0  0  0   0   0   0 0 0 0  1  0  0  dt   0   0]
+  //  [0 0 0  0  0  0   0   0   0 0 0 0  0  1  0   0  dt   0]
+  //  [0 0 0  0  0  0   0   0   0 0 0 0  0  0  1   0   0  dt]
+  //  [0 0 0  0  0  0   0   0   0 0 0 0  0  0  0   1   0   0]
+  //  [0 0 0  0  0  0   0   0   0 0 0 0  0  0  0   0   1   0]
+  //  [0 0 0  0  0  0   0   0   0 0 0 0  0  0  0   0   0   1]
+  // position
+  KF.transitionMatrix.at<double>(0,3) = dt;
+  KF.transitionMatrix.at<double>(1,4) = dt;
+  KF.transitionMatrix.at<double>(2,5) = dt;
+  KF.transitionMatrix.at<double>(3,6) = dt;
+  KF.transitionMatrix.at<double>(4,7) = dt;
+  KF.transitionMatrix.at<double>(5,8) = dt;
+  KF.transitionMatrix.at<double>(0,6) = 0.5*pow(dt,2);
+  KF.transitionMatrix.at<double>(1,7) = 0.5*pow(dt,2);
+  KF.transitionMatrix.at<double>(2,8) = 0.5*pow(dt,2);
+  // orientation
+  KF.transitionMatrix.at<double>(9,12) = dt;
+  KF.transitionMatrix.at<double>(10,13) = dt;
+  KF.transitionMatrix.at<double>(11,14) = dt;
+  KF.transitionMatrix.at<double>(12,15) = dt;
+  KF.transitionMatrix.at<double>(13,16) = dt;
+  KF.transitionMatrix.at<double>(14,17) = dt;
+  KF.transitionMatrix.at<double>(9,15) = 0.5*pow(dt,2);
+  KF.transitionMatrix.at<double>(10,16) = 0.5*pow(dt,2);
+  KF.transitionMatrix.at<double>(11,17) = 0.5*pow(dt,2);
+       /* MEASUREMENT MODEL */
+  //  [1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+  //  [0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+  //  [0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+  //  [0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0]
+  //  [0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0]
+  //  [0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0]
+  KF.measurementMatrix.at<double>(0,0) = 1;  // x
+  KF.measurementMatrix.at<double>(1,1) = 1;  // y
+  KF.measurementMatrix.at<double>(2,2) = 1;  // z
+  KF.measurementMatrix.at<double>(3,9) = 1;  // roll
+  KF.measurementMatrix.at<double>(4,10) = 1; // pitch
+  KF.measurementMatrix.at<double>(5,11) = 1; // yaw
+}
+
+
+void fillMeasurements( cv::Mat &measurements,
+                   const cv::Mat &translation_measured, const cv::Mat &rotation_measured)
+{
+    // Convert rotation matrix to euler angles
+    cv::Mat measured_eulers(3, 1, CV_64F);
+    measured_eulers = rot2euler(rotation_measured);
+    // Set measurement to predict
+    measurements.at<double>(0) = translation_measured.at<double>(0); // x
+    measurements.at<double>(1) = translation_measured.at<double>(1); // y
+    measurements.at<double>(2) = translation_measured.at<double>(2); // z
+    measurements.at<double>(3) = measured_eulers.at<double>(0);      // roll
+    measurements.at<double>(4) = measured_eulers.at<double>(1);      // pitch
+    measurements.at<double>(5) = measured_eulers.at<double>(2);      // yaw
+}
+
+
+void updateKalmanFilter( cv::KalmanFilter &KF, cv::Mat &measurement,
+                     cv::Mat &translation_estimated, cv::Mat &rotation_estimated )
+{
+    // First predict, to update the internal statePre variable
+    cv::Mat prediction = KF.predict();
+    // The "correct" phase that is going to use the predicted value and our measurement
+    cv::Mat estimated = KF.correct(measurement);
+    // Estimated translation
+    translation_estimated.at<double>(0) = estimated.at<double>(0);
+    translation_estimated.at<double>(1) = estimated.at<double>(1);
+    translation_estimated.at<double>(2) = estimated.at<double>(2);
+    // Estimated euler angles
+    cv::Mat eulers_estimated(3, 1, CV_64F);
+    eulers_estimated.at<double>(0) = estimated.at<double>(9);
+    eulers_estimated.at<double>(1) = estimated.at<double>(10);
+    eulers_estimated.at<double>(2) = estimated.at<double>(11);
+    // Convert estimated quaternion to rotation matrix
+    rotation_estimated = euler2rot(eulers_estimated);
 }
